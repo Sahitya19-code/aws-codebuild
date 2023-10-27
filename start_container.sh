18215d338efcb857a79bec7c5a4f064a03dfd8a4
Sahitya Dola <78548003+Sahitya19-code@users.noreply.github.com>
2023-10-27 12:12:20 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull sahitya19/aws-codebuild
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 sahitya19/aws-codebuild
